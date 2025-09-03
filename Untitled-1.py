898666bbe77ce6006d1e330d6b28c69e4939e0d6
saraemadhamdy61-blip <saraemadhamdy61@gmail.com>
2025-09-03 07:20:23 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+temp=int(input("enter the current temperature in celsius: "))
+if temp >=30:
+    print("it is a hot day. stay hydrated!")
+elif 20 <= temp <= 29:
+    print("it is a warm day. enjoy the weathher!")
+elif 10<= temp <=19:
+    print("it is a cool day. wear a jacjet! ")
+else:
+    print("it is a cold day. stay warm! ")            
